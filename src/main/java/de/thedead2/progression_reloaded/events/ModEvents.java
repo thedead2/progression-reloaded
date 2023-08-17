da8eb4b54376ac0d842b651866d7e2da50c1ef25
The_Dead_2 <thedead2@users.noreply.github.com>
2023-08-17 14:10:21 +0200

    Starting to add events to the mod

---
+++ src/main/java/de/thedead2/progression_reloaded/events/ModEvents.java
@@ -0,0 +1,4 @@
+package de.thedead2.progression_reloaded.events;
+
+public class ModEvents {
+}
---
+++ src/main/java/de/thedead2/progression_reloaded/events/ProgressionEvent.java
@@ -0,0 +1,6 @@
+package de.thedead2.progression_reloaded.events;
+
+import net.minecraftforge.eventbus.api.Event;
+
+public class ProgressionEvent extends Event {
+}
